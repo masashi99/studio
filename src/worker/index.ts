36d7830016043d135f6a436ddb0d56bcdea9646c
masashi99 <sgmdq601@gmail.com>
2025-07-13 15:08:39 +0900

    test

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -2,6 +2,6 @@ import { Hono } from "hono";
 
 const app = new Hono<{ Bindings: Env }>();
 
-app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
+app.get("/api/", (c) => c.json({ name: "hoge" }));
 
 export default app;
